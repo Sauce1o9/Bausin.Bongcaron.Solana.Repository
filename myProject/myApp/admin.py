028180a62b222973bbdc4d5b0c899cb4dbb671f3
Kent Jose Bausin <151416035+Sauce1o9@users.noreply.github.com>
2024-11-20 05:02:11 +0800

    Delete myProject directory

--- myProject/myApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin # type: ignore
-from .models import Orders, Customer, Restaurant, Delivery_Driver, Menu# Ensure Customer is imported
-
-# Register your models here.
-admin.site.register(Orders)
-admin.site.register(Customer)
-admin.site.register(Restaurant)
-admin.site.register(Delivery_Driver)
-admin.site.register(Menu)
