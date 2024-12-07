2c7e47867b744d7bd4feeec24c4f1b8d5c601748
Sauce1o9 <151416035+Sauce1o9@users.noreply.github.com>
2024-12-08 00:47:50 +0800

    Added import and export package

--- myProject/myApp/admin.py
+++ myProject/myApp/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin # type: ignore
 from .models import Orders, Customer, Delivery_Driver, Menu, Checkout# Ensure Customer is imported
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 admin.site.register(Orders)
 admin.site.register(Customer)
-admin.site.register(Delivery_Driver)
-admin.site.register(Menu)
 admin.site.register(Checkout)
+
+class MenuAdmin(ImportExportModelAdmin):
+    ...
+
+admin.site.register(Menu, MenuAdmin)
+
+class Delivery_DriverAdmin(ImportExportModelAdmin):
+    ...
+
+admin.site.register(Delivery_Driver, Delivery_DriverAdmin)
--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myApp',
+    'import_export',
 ]
 
 MIDDLEWARE = [
